34b5d628243d8d64393c94b9f308664c8d714406
Edisson Mata <edison20002503@gmail.com>
2023-07-28 17:25:39 -0400

    SplashScreenView created

---
+++ pokeapp/Screens/SplashScreenView.swift
@@ -0,0 +1,41 @@
+//
+//  SplashScreenView.swift
+//  pokeapp
+//
+//  Created by Edisson Mata on 28/7/23.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    var body: some View {
+        ZStack {
+            LinearGradient(
+                colors: [
+                    .customBlue,
+                    .customLightBlue,
+                    .customLightGreen,
+                    .customGreen
+                ],
+                startPoint: .leading,
+                endPoint: .trailing
+            )
+            .ignoresSafeArea()
+            Color.customSnow
+                .opacity(0.8)
+                .ignoresSafeArea()
+            Image("pokeball")
+                .resizable()
+                .frame(width: 105, height: 105)
+                .foregroundColor(.customSnow.opacity(0.8))
+            
+                
+        }
+    }
+}
+
+struct SplashScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashScreenView()
+    }
+}
---
+++ pokeapp/Utils/ColorExtension.swift
@@ -0,0 +1,17 @@
+//
+//  CustomColors.swift
+//  pokeapp
+//
+//  Created by Edisson Mata on 28/7/23.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let customBlue = Color("ColorBlue")
+    static let customLightBlue = Color("ColorLightBlue")
+    static let customLightGreen = Color("ColorLightGreen")
+    static let customGreen = Color("ColorGreen")
+    static let customSnow = Color("ColorSnow")
+}
