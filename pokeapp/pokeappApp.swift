4e047a598e696fb921aaa6e63a7a4b6b683e061b
Edisson Mata <edison20002503@gmail.com>
2023-07-28 15:49:18 -0400

    Initial Commit

---
+++ pokeapp/pokeappApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokeappApp.swift
+//  pokeapp
+//
+//  Created by Edisson Mata on 28/7/23.
+//
+
+import SwiftUI
+
+@main
+struct pokeappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
